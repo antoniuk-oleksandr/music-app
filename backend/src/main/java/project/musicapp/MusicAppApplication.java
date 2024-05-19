fb11d2656fba10e105fb8fc8d9c09629cf327b16
kawaiinex <antonyk063@gmail.com>
2024-05-19 18:19:10 +0300

    init project

---
+++ backend/src/main/java/project/musicapp/MusicAppApplication.java
@@ -0,0 +1,12 @@
+package project.musicapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicAppApplication.class, args);
+	}
+}
---
+++ backend/src/test/java/project/musicapp/MusicAppApplicationTests.java
@@ -0,0 +1,13 @@
+package project.musicapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
