e590476513ac3d4dcb760f69d74301e645294d4c
lynnyk821 <lynnyk265@gmail.com>
2024-05-21 17:51:52 +0300

    delete user playlist dto

--- backend/src/main/java/project/musicapp/api/users/dto/UserPlaylistsDTO.java
+++ backend/src/main/java/project/musicapp/api/users/dto/UserPlaylistsDTO.java
@@ -1,8 +1,5 @@
 package project.musicapp.api.users.dto;
 
-import project.musicapp.api.playlists.PlaylistDTO;
-
 public class UserPlaylistsDTO {
     private UserDTO user;
-    private PlaylistDTO[] playlists;
 }
