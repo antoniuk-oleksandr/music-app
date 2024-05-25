8a232934ff200e94eed7de0c41e3ab76a65a8433
lynnyk821 <lynnyk265@gmail.com>
2024-05-25 10:58:05 +0300

    change user_id to creator_id in PlaylistQuerySQL

--- backend/src/main/java/project/musicapp/api/playlists/utils/PlaylistQuerySQL.java
+++ backend/src/main/java/project/musicapp/api/playlists/utils/PlaylistQuerySQL.java
@@ -2,9 +2,10 @@ package project.musicapp.api.playlists.utils;
 
 public class PlaylistQuerySQL {
     public static final String FIND_ALL_PLAYLISTS_BY_NAME =
-            "SELECT p.playlist_id, p.is_public, p.creating_date, p.name, p.image_path, u.user_id, u.username " +
+            "SELECT p.playlist_id, p.is_public, p.creating_date, p.name, p.image_path, " +
+            "u.user_id, u.username " +
             "FROM playlists AS p " +
-            "JOIN users AS u ON p.user_id = u.user_id " +
+            "JOIN users AS u ON p.creator_id = u.user_id " +
             "WHERE LOWER(p.name) LIKE LOWER(CONCAT('%', :value, '%')) " +
             "LIMIT :limit OFFSET :offset";
 
